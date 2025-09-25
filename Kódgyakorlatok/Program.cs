4098e39d93de4059309da4bfa51a50e6c4d5f0c7
Bálint <szilagyib1@kkszki.hu>
2025-09-25 08:00:16 +0200

    1. feladat commitja

--- Kódgyakorlatok/Program.cs
+++ Kódgyakorlatok/Program.cs
@@ -10,6 +10,38 @@ namespace Kódgyakorlatok
     {
         static void Main(string[] args)
         {
+            //új gyakorlatok 
+            //Készíts egy 20 elemű egy dimenziós egész számokból álló tömböt
+            //Töltsd fel 20 és 30 közé eső véletlen számokkal
+            //határozd meg a tömb legkisebb és legnagyobb elemét (a tanult programozási tételekből)
+            Random rnd = new Random();
+            int[] numbers = new int [20]; 
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(20, 30);
+            }
+
+            int maxszam = numbers[0];
+            int kisszam = numbers[0];
+
+            foreach (var item in numbers)
+            {
+                if (item > maxszam)
+                {
+                    maxszam = item;
+                }
+            }
+            foreach (var item in numbers)
+            {
+                if (item < kisszam)
+                {
+                    kisszam = item;
+                }
+            }
+            Console.WriteLine($"A legnagyobb szám a tömbben: {maxszam}");
+            Console.WriteLine($"A legkisebb elem a tömbben: {kisszam}");
+            Console.ReadLine();
         }
     }
 }
